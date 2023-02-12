03acf5342e8441dd58cb575cc558e784bfffb874
Will <70348218+imaginary-png@users.noreply.github.com>
2023-02-12 11:06:40 +1100

    rough layout of logic

--- tic-tac-toe/tictactoe.js
+++ tic-tac-toe/tictactoe.js
@@ -9,42 +9,73 @@ const gameBoard = ["", "", "", "", "", "", "", "", ""];
 // totalMoves
 // state = playing, won
 
+// or create a 'game' object that has a gameBoard and two players?
+// basically a viewModel, with 2 models
+
 function createPlayer(name, icon) {
-  // name
-  // piece / icon
-  // wins
-  // losses
+    // name
+    // piece / icon
+    // wins
+    // losses
 }
 
 ///
 
+/*
+    Functions for playing the game
+        and making moves
+*/
 function updateBoardInfo() {
-  // update the html of the game info
-  // current player to move..
-  // current move..
-  // etc.
+    // update the html of the game info
+    // current player to move..
+    // current move..
+    // etc.
 }
 
 function makeMove(player) {
-  const move = getMove(player);
-  gameBoard.makeMove(player.piece, move);
-  updateBoardInfo();
-  // return true if game won with this move.
-  if (gameBoard.state === "won") return true;
-  return false;
+    const move = getMove(player);
+    gameBoard.makeMove(player.piece, move);
+    updateBoardInfo();
+    // return true if game won with this move.
+    if (gameBoard.state === "won") return true;
+    return false;
 }
 
 function getMove(player) {
-  // validates and return the move...
+    // validates and return the move...
 }
 
 function gameLoop() {
-  if (makeMove(playerOne)) return;
-  if (makeMove(playerTwo)) return;
+    if (makeMove(playerOne)) return;
+    if (makeMove(playerTwo)) return;
 }
 
 function startGame() {
-  while (gameBoard.state === "playing") {
-    gameLoop();
-  }
+    while (gameBoard.state === "playing") {
+        gameLoop();
+    }
+    showWinner();
+    showPlayButton();
 }
+
+/* 
+    Subscribe to the click evens on the game board / grid
+ */
+function subscribeGameboardClicks() {
+    const board = document.getElementsByClassName("gameBoardSquare");
+    board.forEach((square) => {
+        square.addEventListener("click", (e) => {
+            e.preventDefault();
+            game.MakeMove();
+        });
+    });
+}
+
+const playButton = document.getElementById("play_button");
+playButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    // hide button
+    startGame();
+});
+
+subscribeGameboardClicks();
