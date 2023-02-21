c7b3fe792eb0daee7d31c59c12bd45f8bb6c2cc4
Will <70348218+imaginary-png@users.noreply.github.com>
2023-02-21 15:16:16 +1100

    add: comments

--- recursion/merge-sort/merge-sort.js
+++ recursion/merge-sort/merge-sort.js
@@ -1,15 +1,21 @@
 function mergeSort(arr) {
+    // if the sub-array has been broken down into a singular number, return.
     if (arr.length === 1) return arr;
 
+    // otherwise find, 'mid' point, and break down further.
     const mid = arr.length / 2;
     let left = arr.slice(0, mid);
     let right = arr.slice(mid);
+    // recursive calls
     left = mergeSort(left);
     right = mergeSort(right);
 
+    // once recursive calls returned, sort the sub-arrays.
+    // when the initial array length is uneven, some sub-array comparisons will have different lengths.
     const result = [];
-
+    // while both sub-arrays have values remaining.
     while (left.length > 0 && right.length > 0) {
+        // compare left and right value, then push to result, and shift off (removes from beginning of the arr.) from sub-array.
         if (left[0] < right[0]) {
             result.push(left.shift());
         } else {
@@ -17,7 +23,7 @@ function mergeSort(arr) {
         }
     }
 
-    // in case on uneven sub-arrays, push any remaining values
+    // finally, push any remaining values onto result. If sub-array has no values, nothing will be added.
     result.push(...left, ...right);
 
     return result;
