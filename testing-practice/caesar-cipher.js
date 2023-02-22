40386d751caf22359fb6edda2b6a2913ca191043
Will <70348218+imaginary-png@users.noreply.github.com>
2023-02-22 19:12:31 +1100

    add: caesar-cipher

--- testing-practice/caesar-cipher.js
+++ testing-practice/caesar-cipher.js
@@ -1,1 +1,22 @@
-export default function cipher(text, shift) {}
+// ASCII uppercase : 65-90
+//       lowercase : 97-122
+// https://www.rapidtables.com/code/text/ascii-table.html
+export default function cipher(text, shift) {
+    if (typeof text !== "string") return undefined;
+
+    shift %= 26;
+    const result = text.split("").reduce((res, c) => {
+        // if c is not [a-zA-Z] don't shift the character
+        if (c < "A" || (c > "Z" && c < "a") || c > "z") return res + c;
+        // otherwise, shift the char code and append the new character to the accumulator
+        let charCode = c.charCodeAt(0) + shift;
+        // if char greater than z ||  if char less than A, greater than Z, loop around
+        if (charCode > 122 || (charCode < 97 && charCode > 90)) {
+            charCode -= 26;
+        }
+        const shiftedChar = String.fromCharCode(charCode);
+        return res + shiftedChar;
+    }, "");
+
+    return result;
+}
--- testing-practice/caesar-cipher.test.js
+++ testing-practice/caesar-cipher.test.js
@@ -4,23 +4,23 @@ test("cipher: 'hello world' shift: 5", () => {
     expect(cipher("hello world", 5)).toBe("mjqqt btwqi");
 });
 test("cipher: 'Hello World!' shift: 5", () => {
-    expect(cipher("Hello world!", 5)).toBe("Mjqqt Btwqi!");
+    expect(cipher("Hello World!", 5)).toBe("Mjqqt Btwqi!");
 });
 test("cipher: 'Hello World!' shift: 5", () => {
-    expect(cipher("Hello world!", 5)).toBe("Mjqqt Btwqi!");
+    expect(cipher("Hello World!", 5)).toBe("Mjqqt Btwqi!");
 });
 test("cipher: 'Hello World!' shift: 0", () => {
-    expect(cipher("Hello world!", 0)).toBe("Hello World!");
+    expect(cipher("Hello World!", 0)).toBe("Hello World!");
 });
 test("cipher: 'Hello World!' shift: 26", () => {
-    expect(cipher("Hello world!", 26)).toBe("Hello World!");
+    expect(cipher("Hello World!", 26)).toBe("Hello World!");
 });
-test("cipher: 'Hello World!' shift: 30", () => {
-    expect(cipher("Hello world!", 30)).toBe("Mjqqt Btwqi!");
+test("cipher: 'Hello World!' shift: 31", () => {
+    expect(cipher("Hello World!", 31)).toBe("Mjqqt Btwqi!");
 });
 test("cipher: '1234' shift: 3", () => {
     expect(cipher("1234", 3)).toBe("1234");
 });
 test("non-string cipher: 3.1415 shift: 3", () => {
-    expect(cipher("1234", 3)).toBeUndefined();
+    expect(cipher(3.1415, 3)).toBeUndefined();
 });
