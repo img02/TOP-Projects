e9d993aef63ef36ce20524cee80f144b7d80f810
Will <70348218+imaginary-png@users.noreply.github.com>
2023-02-22 17:19:05 +1100

    add: capitalise and tests

--- testing-practice/capitalise.test.js
+++ testing-practice/capitalise.test.js
@@ -9,7 +9,7 @@ test("Capitalise sentence: 'goodbye world!'", () => {
 test("Capitalise turns all other letters to lowercase: 'HELLO world!'", () => {
     expect(capitalise("HELLO world!")).toBe("Hello world!");
 });
-test("Capitalise non-character does nothing: '123'", () => {
+test("Capitalise non-alphabetical character does nothing: '123'", () => {
     expect(capitalise("123")).toBe("123");
 });
 test("Capitalise non-string returns error msg: 456.123", () => {
